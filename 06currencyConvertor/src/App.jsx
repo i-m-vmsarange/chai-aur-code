395af9ddaf930261e438b3c091d5f15230c32ce1
VAISHNAVI SARANGE <sarangevaishnavi55@gmail.com>
2026-07-23 15:51:39 +0530

    started creating currency converter

---
+++ 06currencyConvertor/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+  <>
+  <h1 className='text-2xl font-semibold bg-amber-500 text-center'>Currency Converter</h1>
+  </>
+  )
+}
+
+export default App
