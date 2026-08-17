18b8b536238b14e9b727e3c54da6e4326bde0777
VAISHNAVI SARANGE <sarangevaishnavi55@gmail.com>
2026-08-17 12:15:04 +0530

    saved cookie at browser side

--- 07auth/src/app.js
+++ 07auth/src/app.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const authRouter = require("./routes/auth.routes");
-
 const app = express();
-
+const cookieParser = require("cookie-parser");
 
 app.use(express.json());
-app.use("/api/auth", authRouter)
+app.use("/api/auth", authRouter);
+app.use(cookieParser());
 
 module.exports = app;
--- 07auth/src/controllers/auth.controller.js
+++ 07auth/src/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 const userModel = require("../models/user.model");
-
+const crypto = require("crypto");
+const jwt = require("jsonwebtoken");
 
 async function registerUser(req, res) {
 
@@ -10,18 +11,23 @@ async function registerUser(req, res) {
         const doesUserExists = await userModel.findOne({ username });
 
         if (doesUserExists) {
-
             res.status(409).json({
                 message: "User already exists"
             })
         }
         const user = await userModel.create({
-            username, email, password
+            username,
+            email,
+            password: crypto.createHash("sha256").update(password).digest("hex")
         });
+
+        const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: "1h" });
+
+        res.cookie("token", token, { httpOnly: true, secure: true });
 
         res.status(200).json({
             message: "User created successfully",
-            user: user
+            user: user,
         })
     }
     catch (error) {
