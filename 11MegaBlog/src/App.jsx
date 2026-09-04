066022e3c557d9d11b3295172c069b73dd79a252
VAISHNAVI SARANGE <sarangevaishnavi55@gmail.com>
2026-09-04 17:44:21 +0530

    configured appwrite

--- 11MegaBlog/src/App.jsx
+++ 11MegaBlog/src/App.jsx
@@ -1,7 +1,8 @@
 import React from "react";
 
 const App = () => {
-  return <div>App</div>;
+  console.log(import.meta.env.VITE_APPWRITE_URL);
+  return <div className="font-bold text-center p-4 text-4xl">App</div>;
 };
 
 export default App;
