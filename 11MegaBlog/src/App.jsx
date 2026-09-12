1b9509bb5c87fc64fe17c97d596df3aa495ac079
VAISHNAVI SARANGE <sarangevaishnavi55@gmail.com>
2026-09-12 17:56:33 +0530

    WIP: building components

--- 11MegaBlog/src/App.jsx
+++ 11MegaBlog/src/App.jsx
@@ -1,8 +1,40 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useDispatch } from "react-redux";
+import authService from "./appwrite/auth";
+import { login, logout } from "./features/auth/authSlice";
+import { Footer } from "./components";
 
 const App = () => {
-  console.log(import.meta.env.VITE_APPWRITE_URL);
-  return <div className="font-bold text-center p-4 text-4xl">App</div>;
+  const [loading, setLoading] = useState(true);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    authService
+      .getCurrentUser()
+      .then((userData) => {
+        if (userData) {
+          dispatch(login({ userData }));
+        } else {
+          dispatch(logout());
+        }
+      })
+      .catch((error) => {
+        console.log("Error: ", error);
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  });
+
+  return !loading ? (
+    <div className="min-h-screen flex flex-wrap content-between bg-grey-400">
+      <div className="min-w-full block">
+        <Header />
+        <main>{/* TODO: <Outlet /> */}</main>
+        <Footer />
+      </div>
+    </div>
+  ) : null;
 };
 
 export default App;
