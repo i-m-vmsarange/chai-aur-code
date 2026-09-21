0f8878c59ebc6b0bea531a14f0dff72dfdbd5856
VAISHNAVI SARANGE <sarangevaishnavi55@gmail.com>
2026-09-21 22:42:00 +0530

    math and nums

--- JS-Refresher/01variables/nums_and_math.js
+++ JS-Refresher/01variables/nums_and_math.js
@@ -1,14 +1,30 @@
-const score = 400;
+// const score = 400;
 
-const balance = Number(400);
+// const balance = Number(400);
 
-console.log(`score: ${score} balance: ${balance}`);
-console.log(`balance: ${balance.toString()}`);
-console.log(balance.toFixed(1));
+// console.log(`score: ${score} balance: ${balance}`);
+// console.log(`balance: ${balance.toString()}`);
+// console.log(balance.toFixed(1));
 
-const otherNumber = 23.8999;
+// const otherNumber = 23.8999;
 
-console.log(`otherNumber: ${otherNumber.toPrecision(2)}`);
+// console.log(`otherNumber: ${otherNumber.toPrecision(2)}`);
 
-const hundreds = 1000000;
-console.log(`hundreds: ${hundreds.toLocaleString("en-IN")}`);
+// const hundreds = 1000000;
+// console.log(`hundreds: ${hundreds.toLocaleString("en-IN")}`);
+
+// +++++++++++++++++++++ MATHS +++++++++++++++++++++++++++++++++++++++++++++
+
+// console.log(Math); // Object
+// console.log(Math.abs(-44)); // absolute value
+// console.log(Math.round(4.1)); // 4
+// console.log(Math.ceil(4.1)); //5
+// console.log(Math.floor(4.9)); //4
+
+// console.log(Math.floor(Math.random() * 10) + 1);
+
+const min = 10;
+const max = 20;
+
+//                                      20 - 10 + 1 = 11
+console.log(Math.floor(Math.random() * (max - min + 1)) + min);
--- JS-Refresher/01variables/strings.js
+++ JS-Refresher/01variables/strings.js
@@ -2,20 +2,20 @@
 // const repoCount = 50;
 // console.log(`Hello ${name}, how are you? I think you have over ${repoCount} repositories on your github`);
 
-const gameName = new String('hiteshhc')
+const gameName = new String("hiteshhc");
 console.log(`gameName: ${gameName}`);
 console.log(gameName.length);
 console.log(gameName.toUpperCase());
-console.log(gameName.toLowerCase())
+console.log(gameName.toLowerCase());
 console.log(gameName.charAt(2)); //t
-console.log(gameName.indexOf('t')); //2
+console.log(gameName.indexOf("t")); //2
 console.log(gameName.__proto__);
 console.log(gameName.split("e")); //'hit' 'shhc'
 
 const subString = gameName.substring(0);
 console.log(`new String: ${subString}`);
 
-const sliceString = gameName.slice(-8, 4)
+const sliceString = gameName.slice(-8, 4);
 console.log(`sliceString: ${sliceString}`); //
 
 const trimString = "   hitesh   ";
@@ -23,3 +23,6 @@ console.log(`trimString: ${trimString.trim()}`);
 
 const url = "https://hitesh.com/hitesh%20choudhary";
 console.log(url.replaceAll("%20", "-"));
+
+console.log(url.includes("divya"));
+console.log(url.split("/"));
